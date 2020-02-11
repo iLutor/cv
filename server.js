cd8e25b1efa9e3ae73cfd5335209f7362b7d0ab1
ilutor <lutor1106@gmail.com>
2020-02-11 01:57:39 -0300

    3d assets

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ console.log(__dirname);
 app.use(express.static(__dirname + '/'));
 
 
-app.get('/*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 
 });
 
+app.get('/3d', function (req, res) {
+  res.sendFile(path.join(__dirname + '/lutor.html'));
+
+});
 app.listen(process.env.PORT || 8000);
