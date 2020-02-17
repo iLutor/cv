9ca4c76787252f49c2e4c45461f49e6a156f46dd
ilutor <lutor1106@gmail.com>
2020-02-17 00:09:19 -0300

    back to top button

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,54 @@
 $(document).ready(function(){
 
+    ///////////////////////////
+    // //Get the button
+    var mybutton = document.getElementById("back-to-top");
+
+    // // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+         mybutton.style.display = "block";
+     } else {
+         mybutton.style.display = "none";
+     }
+    }
+	// Smooth scroll
+	$("#nav .main-nav a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		var hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 600);
+	});
+
+	$('#back-to-top').on('click', function(){
+		$('body,html').animate({
+			scrollTop: 0
+		}, 600);
+    });
+    ///////////////////////////
+
+    // //Get the button
+    // var mybutton = document.getElementById("back-to-top");
+
+    // // When the user scrolls down 20px from the top of the document, show the button
+    // window.onscroll = function() {scrollFunction()};
+
+    // function scrollFunction() {
+    // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    //     mybutton.style.display = "block";
+    // } else {
+    //     mybutton.style.display = "none";
+    // }
+    // }
+
+    // // When the user clicks on the button, scroll to the top of the document
+    // function topFunction() {
+    // document.body.scrollTop = 0;
+    // document.documentElement.scrollTop = 0;
+    // }
     var specialElementHandler = {
         "#editor":function(element,renderer){
             return true;
