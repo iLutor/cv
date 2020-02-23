a456aa2704698c9af2930ce49300af93315e6daa
ilutor <lutor1106@gmail.com>
2020-02-23 18:19:35 -0300

    change file locatons

--- server.js
+++ server.js
@@ -7,13 +7,32 @@ app.use(express.static(__dirname + '/'));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-
+  res.sendFile(path.join(__dirname + '/pages/index.html'));
 });
+
+app.get('/cv', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/cv_personal.html'));
+});
+
 
 app.get('/3d', function (req, res) {
-  //res.sendFile(path.join(__dirname + '/lutor.html'));
-  res.sendFile(path.join(__dirname + '/cubic_images.html'));
-
+  res.sendFile(path.join(__dirname + '/pages/cubic_images.html'));
 });
+
+app.get('/blog', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/blog.html'));
+});
+
+app.get('/e-commerce', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/e_commerce.html'));
+});
+
+app.get('/find_nearyou', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/find_nearyou.html'));
+});
+
+app.get('/apps', function (req, res) {
+  res.sendFile(path.join(__dirname + '/pages/apps.html'));
+});
+
 app.listen(process.env.PORT || 8000);
