8a2880637e7a9a41103fb37a551e9cb6cf7582ed
ilutor <lutor1106@gmail.com>
2022-07-22 20:47:11 -0400

    changes files

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     ///////////////////////////
     // //Get the button
-    var mybutton = document.getElementById("back-to-top");
+    let mybutton = document.getElementById("back-to-top");
     // // When the user scrolls down 20px from the top of the document, show the button
     window.onscroll = function() {scrollFunction()};
 
@@ -19,17 +19,9 @@ $(document).ready(function(){
 			scrollTop: 0
 		}, 600);
     });
-    ///////////////////////////
-
-    var specialElementHandler = {
-        "#editor":function(element,renderer){
-            return true;
-        }
-    };
 
     $("#cmd-print").click(function(){
         window.print();
     });  
-
  
 });
